60ca79e9bc891ed17eaa02044cc68b1ad164d758
slongle <1050106988@qq.com>
2020-04-12 10:21:31 +0800

    Add conductor material and support SPD spectrum

---
+++ src/bsdf/conductor.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "core/primitive.h"
+#include "bsdfutil.h"
+
+class SmoothConductor :public BSDF {
+public:
+    SmoothConductor(
+        const std::shared_ptr<Texture<Spectrum>>& reflectance,
+        const Spectrum& eta, const Spectrum& k) 
+        :m_reflectance(reflectance), m_eta(eta), m_k(k) {
+        std::cout << eta << std::endl;
+        std::cout << k << std::endl;
+    }    
+
+    Spectrum Sample(MaterialRecord& matRec, Float2 s) const {
+        if (Frame::CosTheta(matRec.m_wi) <= 0) {            
+            return Spectrum(0.f);
+        }
+
+        matRec.m_wo = Reflect(matRec.m_wi);
+        matRec.m_pdf = 1.f;
+        return m_reflectance->Evaluate(matRec.m_st) * 
+            FresnelConductor(Frame::CosTheta(matRec.m_wi), m_eta, m_k);
+    }
+    Spectrum Eval(MaterialRecord& matRec) const {
+        if (Frame::CosTheta(matRec.m_wi) <= 0 ||
+            Frame::CosTheta(matRec.m_wo) <= 0 ||
+            std::abs(Dot(Reflect(matRec.m_wi), matRec.m_wo) - 1) > deltaEpsilon) {
+            return Spectrum(0.f);
+        }
+        return m_reflectance->Evaluate(matRec.m_st) *
+            FresnelConductor(Frame::CosTheta(matRec.m_wi), m_eta, m_k);
+    }
+    float Pdf(MaterialRecord& matRec) const { 
+        if (false || Frame::CosTheta(matRec.m_wi) <= 0 ||
+            Frame::CosTheta(matRec.m_wo) <= 0 ||
+            std::abs(Dot(Reflect(matRec.m_wi), matRec.m_wo) - 1) > deltaEpsilon) {
+            return matRec.m_pdf = 0.f;
+        }
+        return matRec.m_pdf = 1.f;        
+    }
+    Spectrum EvalPdf(MaterialRecord& matRec) const { 
+        if (Frame::CosTheta(matRec.m_wi) <= 0 ||
+            Frame::CosTheta(matRec.m_wo) <= 0 ||
+            std::abs(Dot(Reflect(matRec.m_wi), matRec.m_wo) - 1) > deltaEpsilon) {
+            matRec.m_pdf = 0.f;
+            return Spectrum(0.f);
+        }
+        matRec.m_pdf = 1.f; 
+        return m_reflectance->Evaluate(matRec.m_st) *
+            FresnelConductor(Frame::CosTheta(matRec.m_wi), m_eta, m_k);
+    }
+    bool IsDelta() const { return true; }
+private:
+    Float3 Reflect(const Float3& v) const {
+        return Float3(-v.x, -v.y, v.z);
+    }
+
+    Spectrum m_eta, m_k;
+    std::shared_ptr<Texture<Spectrum>> m_reflectance;
+};
--- src/core/spectrum.h
+++ src/core/spectrum.h
@@ -7,10 +7,21 @@
 #include "color.h"
 #include "vector.h"
 
+inline float Sqrt(const float& a) { return std::sqrt(std::max(0.f, a)); }
+
+static const int nCIESamples = 471;
+extern const float CIE_X[nCIESamples];
+extern const float CIE_Y[nCIESamples];
+extern const float CIE_Z[nCIESamples];
+extern const float CIE_lambda[nCIESamples];
+static const float CIE_Y_integral = 106.856895;
+
 class RGBSpectrum {
 public:
     RGBSpectrum(float v = 0) :r(v), g(v), b(v) {}
     RGBSpectrum(float _r, float _g, float _b) :r(_r), g(_g), b(_b) {}
+    RGBSpectrum(const float* v) : r(v[0]), g(v[1]), b(v[2]) {}
+    RGBSpectrum(const std::string& filename);
     RGBSpectrum(const Float3& v) :r(std::fabs(v.x)), g(std::fabs(v.y)), b(std::fabs(v.z)) {}
 
     RGBSpectrum operator * (float v) const { return RGBSpectrum(r * v, g * v, b * v); }
@@ -19,24 +30,26 @@ public:
 
     RGBSpectrum operator + (const RGBSpectrum& s) const { return RGBSpectrum(r + s.r, g + s.g, b + s.b); }
     RGBSpectrum& operator += (const RGBSpectrum& s) { r += s.r; g += s.g; b += s.b; return *this; }
+    RGBSpectrum operator - (const RGBSpectrum& s) const { return RGBSpectrum(r - s.r, g - s.g, b - s.b); }
     RGBSpectrum operator * (const RGBSpectrum& v) const { return RGBSpectrum(r * v.r, g * v.g, b * v.b); }
     RGBSpectrum& operator *= (const RGBSpectrum& v) { r *= v.r; g *= v.g; b *= v.b; return *this; }
+    RGBSpectrum operator / (const RGBSpectrum& v) const { return RGBSpectrum(r / v.r, g / v.g, b / v.b); }
 
     float y() const { return (r * 0.299) + (g * 0.587) + (b * 0.114); }
     bool IsBlack() const { return r == 0 && g == 0 && b == 0; }
 
-    sRGB TosRGB() const {
-        return sRGB(GammaCorrect(r), GammaCorrect(g), GammaCorrect(b));
-    }
+    sRGB TosRGB() const { return sRGB(GammaCorrect(r), GammaCorrect(g), GammaCorrect(b)); }
+
+    static RGBSpectrum FromSPD(const float* lambda, const float* val, const int& n);
 
     float r, g, b;
 
+    friend RGBSpectrum Sqrt(const RGBSpectrum& s) { return RGBSpectrum(Sqrt(s.r), Sqrt(s.g), Sqrt(s.b)); }
+    friend float MaxComponent(const RGBSpectrum& s) { return std::max(s.r, std::max(s.g, s.b)); }
     friend std::ostream& operator <<(std::ostream& os, const RGBSpectrum& s) {
         os << s.r << ' ' << s.g << ' ' << s.b;
         return os;
     }
 };
-
-inline float MaxComponent(const RGBSpectrum& s) { return std::max(s.r, std::max(s.g, s.b)); }
 
 typedef RGBSpectrum Spectrum;
