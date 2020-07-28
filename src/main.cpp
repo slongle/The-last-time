1ea3a75b201dc746525fc072f96643a16c2e8354
slongle <1050106988@qq.com>
2020-07-28 16:31:00 +0800

    Remove path prefix

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 int main(int argc, char* argv[]) {
 	google::InitGoogleLogging("Render");
 	google::InstallFailureSignalHandler();
-
-	std::string prefix = "E:/Document/Graphics/code/The-Last-time/scenes/Scenes/";
+	
+	LOG_IF(FATAL, argc < 2) << "Without scenes' path.";
+	std::string prefix(argv[1]);
 	std::vector<std::string> scenes(100);
 	scenes[0] = "Box/Diffuse/scene.json";
 	scenes[1] = "Box/Specular/scene.json";
 	scenes[2] = "1.Texture/Globe/scene.json";
-    scenes[3] = "1.Texture/Pbrt-book/scene.json";
+	scenes[3] = "1.Texture/Pbrt-book/scene.json";
 	scenes[4] = "1.Texture/Leaf/scene.json";
 	scenes[5] = "2.Shader/Shader-ball/scene.json";
 	scenes[6] = "3.Volume/Homogeneous/scene.json";
@@ -19,6 +20,7 @@ int main(int argc, char* argv[]) {
 	scenes[9] = "5.Guide/Torus/scene.json";
 	scenes[10] = "6.Caustics/DiscoBall/scene.json";
 	scenes[11] = "6.Caustics/Water/scene.json";
+	scenes[12] = "6.Caustics/VolumeCaustics/scene.json";
 	std::string filename = prefix + scenes[5];
 
 	Renderer renderer(filename);
