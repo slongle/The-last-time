b38de4406a354853981338ad582f6ee0e972b1b7
slongle <1050106988@qq.com>
2020-08-14 20:34:15 +0800

    Replace stb_image with vcpkg package

--- src/utility/imageio.cpp
+++ src/utility/imageio.cpp
@@ -3,12 +3,12 @@
 // Stb_image
 #ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image/stb_image.h"
+#include <stb_image/stb_image.h>
 #endif // !STB_IMAGE_IMPLEMENTATION
 
 #ifndef STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image/stb_image_write.h"
+#include <stb_image/stb_image_write.h>
 #endif // !STB_IMAGE_WRITE_IMPLEMENTATION
 
 
